b5788ef7fbad7472c9062f50112ab63c870fa1f5
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-12-20 23:52:53 +0530

    scripts

---
+++ DOM/todo-app/script.js
@@ -0,0 +1,10 @@
+// const headerTitle = document.getElementById("header-title");
+// headerTitle.innerText = "Todo title";
+
+// //classes
+// const listItems = document.getElementsByClassName("list-group-item");
+// console.log(listItems); //array
+
+//get elementby tag name
+
+const li = document.getElementsByTagName("li");
