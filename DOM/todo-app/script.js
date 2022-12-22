35ffa6d5b22eaec03624cdeb9fa54671b9ed3b0a
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-12-22 23:14:06 +0530

    js inserted

--- DOM/todo-app/script.js
+++ DOM/todo-app/script.js
@@ -8,3 +8,36 @@
 //get elementby tag name
 
 const li = document.getElementsByTagName("li");
+
+let todos = [];
+
+const todo = document.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = document.getElementById("todo").value;
+
+  if (data == "") {
+    let err = document.getElementById("errormsg");
+    err.textContent = "Please enter some value";
+  } else {
+    todos.push(data);
+
+    console.log(todos);
+
+    let list = document.getElementById("todos");
+
+    let todolist = document.createElement("li");
+    todolist.setAttribute(
+      "class",
+      "list-group-item mb-2 d-flex justify-content-between"
+    );
+    todolist.appendChild(document.createTextNode(data));
+
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    button.setAttribute("class", "btn btn-danger btn-sm");
+    todolist.appendChild(button);
+    list.appendChild(todolist);
+  }
+});
+
+console.log(todos);
