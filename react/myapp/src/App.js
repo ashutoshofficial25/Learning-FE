1db67717c5c3674e862453f26cd4e3cfcdecac40
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-12-30 20:46:51 +0530

    ..burger

---
+++ react/myapp/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* BreadUpper
+BreadLower
+Cheese
+Lettuce
+Tomato */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react/myapp/src/createBurget.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateBurget = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+};
+
+export default CreateBurget;
