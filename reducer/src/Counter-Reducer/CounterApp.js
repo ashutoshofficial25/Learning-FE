f9201aec1ee591fbeb6cdda0d463cd71388482cd
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-18 21:52:47 +0530

    reducer couter

---
+++ reducer/src/Counter-Reducer/CounterApp.js
@@ -0,0 +1,39 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { createContext, useReducer, useState } from "react";
+import Counter from "./components/Counter";
+
+export const CountContext = createContext();
+
+//reducer is pure function
+//reducer function two takes  the` parameters state and action
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "incrementByNum":
+      return state + action.payload;
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+const initialState = 10;
+
+function App() {
+  // const [counter, setCounter] = useState(0);
+
+  // const [updatedState, dispatch('action')] = useReducer(reducer function(state,action) , initialState);)
+  const [counter, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <CountContext.Provider value={{ counter, dispatch }} className="App">
+      <Counter />
+    </CountContext.Provider>
+  );
+}
+
+export default App;
