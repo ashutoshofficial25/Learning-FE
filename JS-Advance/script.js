702d1a3156237ec281ee142502cbbf8c2f878c65
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-02-04 16:13:16 +0530

    debouncing and throttling

--- JS-Advance/script.js
+++ JS-Advance/script.js
@@ -1,11 +1,38 @@
 const input = document.getElementById("input");
 const defaultText = document.getElementById("default-text");
-const debounceText = document.getElementById("debounceText");
-const throttleText = document.getElementById("throttleText");
+const debounceText = document.getElementById("debounce-text");
+const throttleText = document.getElementById("throttle-text ");
 
-console.log(input.value);
+const updateDebounceText = debounce((text) => (debounceText.innerText = text));
+const updateThrottleText = throttle((text) => (throttleText.innerText = text));
 
-input.addEventListener("change", (e) => {
+input.addEventListener("input", (e) => {
   console.log("running");
   defaultText.innerText = e.target.value;
+  updateDebounceText(e.target.value);
+  updateThrottleText(e.target.value);
 });
+
+function debounce(callback, time = 1000) {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      callback(...args);
+    }, time);
+  };
+}
+
+function throttle(callback, time = 500) {
+  let wait = false;
+
+  return (...args) => {
+    if (wait) return;
+
+    callback(...args);
+    wait = true;
+    setTimeout(() => {
+      wait = false;
+    }, time);
+  };
+}
